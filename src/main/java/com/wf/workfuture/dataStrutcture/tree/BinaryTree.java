7f2e9156cdf49d53cd089211b736c7b80b49935c
tomwangfeng <tomwangfeng@didiglobal.com>
2019-06-25 16:32:38 +0800

    fix conflict

--- src/main/java/com/wf/workfuture/dataStrutcture/tree/BinaryTree.java
+++ src/main/java/com/wf/workfuture/dataStrutcture/tree/BinaryTree.java
@@ -20,11 +20,6 @@ public class BinaryTree {
     }
 
     public List<Integer> printTreeRightNode(TreeNode head){
-<<<<<<< HEAD
-
-        List<Integer> list = new ArrayList<>();
-=======
->>>>>>> 2028aaf61c61d9ddd41712b1af2a047fe1a708cd
 
         List<Integer> list = new ArrayList<>();
 
